27e0cb6720676f45865363c1c92aecc92fe641a8
praniil <pparajuli15@gmail.com>
2023-08-14 08:12:57 +0545

    learnt variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ") #expects string 
+nameUser = "Pranil"
+print("hello", name, nameUser)
+
+print(f"hello my name is: {name}")
+print("hello my name is: " + name , nameUser)      # + concationation
+
+# parameter vs argument
+# print(potential arguments) parameters
+# parameters : what function can take 
+# arguments : what we input when we use a function
+
+#print(*objects, spc= ' ', endl="\n")  
+
+print("hello, ", end="")
+print(name)
